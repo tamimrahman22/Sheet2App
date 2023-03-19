2c9e52f6598be7f83234e810ee8f85170a642464
Jonathan Min <39451363+min-j@users.noreply.github.com>
2023-03-19 19:56:22 -0400

    implemented google sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 
 function App() {
+  const [ user, setUser ] = useState({});
+
+  function handleInitalizeCallback(response) {
+    console.log("TOKEN: " + response.credential);
+    var user = jwt_decode(response.credential);
+    console.log(user);
+    setUser(user);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut() {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+  
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "382110346041-m240qc561dnte39gobhn1f0int19uusr.apps.googleusercontent.com",
+      callback: handleInitalizeCallback
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      { user &&
+        <div>
+          <img src={user.picture} alt=""></img>
+          <h3>{user.email}</h3>
+          <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+        </div>
+      }
     </div>
   );
 }
