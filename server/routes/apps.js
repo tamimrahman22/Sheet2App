c873c46890a67cc21dc399c64e748232cd470665
Sabareesh Vishwanathan <sabareesh.vishwanathan@stonybrook.edu>
2023-03-20 10:56:59 -0400

    get list of apps

--- server/routes/apps.js
+++ server/routes/apps.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const App = require('../models/Apps');
+const appModel = require('../models/Apps');
 
 router.post('/create', async(req, res) => {
     const { name, creator, roles, roleMembershipSheet } = req.body;
 	console.log(req.body);
     try {
-		const newApp = await App.create({
+		const newApp = await appModel.create({
 			name: name,
 			creator: creator,
 			roleMembershipSheet: roleMembershipSheet,
@@ -16,8 +16,21 @@ router.post('/create', async(req, res) => {
     }
     catch (error) {
 		console.error('Error: ', error);
-		res.status(500).json({ message: `Error in app creation for app ${name}` });
+		res.status(400).json({ message: `Error in app creation for app ${name}` });
 	}
 });
+
+router.get('/list', async(req, res) => {
+	try {
+		const list = await appModel.find({ });
+		console.log(list);
+		res.send(list);
+	}
+	catch (err) {
+		console.error('Error: ', error);
+		res.status(400).json({ message: `Error in getting apps` });
+	}
+});
+
 
 module.exports = router;
